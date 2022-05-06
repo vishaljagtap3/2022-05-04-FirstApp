073b6ca8fd5d0451b997cb41ac39b5b09ae456fc
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-05-06 13:43:13 +0530

    Commned added!

--- app/src/main/java/in/bitcode/studentapp/MainActivity.java
+++ app/src/main/java/in/bitcode/studentapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    //this is a comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
