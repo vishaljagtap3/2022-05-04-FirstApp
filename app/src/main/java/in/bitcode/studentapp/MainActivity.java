df12b092b9187ac6e7ea0584fdf897324c338049
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-05-07 14:15:11 +0530

    Creating UI through code! And setting up an OnClickListener to Button.

--- app/src/main/java/in/bitcode/studentapp/MainActivity.java
+++ app/src/main/java/in/bitcode/studentapp/MainActivity.java
@@ -1,15 +1,87 @@
 package in.bitcode.studentapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    //this is a comment
+    private EditText edtUsername, edtPassword;
+    private TextView txtWelcomeMessage;
+    private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LinearLayout mainContainerLayout = new LinearLayout(this);
+        mainContainerLayout.setOrientation(LinearLayout.VERTICAL);
+        mainContainerLayout.setPadding(20, 20, 20, 40);
+        mainContainerLayout.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
+
+        ViewGroup.LayoutParams layoutParams =
+                new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                );
+
+        mainContainerLayout.setLayoutParams(layoutParams);
+
+
+        txtWelcomeMessage = new TextView(this);
+        txtWelcomeMessage.setText("Welcome to Android@BitCode");
+        txtWelcomeMessage.setTextSize(30);
+        txtWelcomeMessage.setTextColor(Color.BLACK);
+
+        ViewGroup.LayoutParams paramsForView =
+                new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+
+        txtWelcomeMessage.setLayoutParams(paramsForView);
+
+        mainContainerLayout.addView(txtWelcomeMessage);
+        //setContentView(txtWelcomeMessage);
+
+        edtUsername = new EditText(this);
+        edtUsername.setHint("Enter username");
+        edtUsername.setLayoutParams(paramsForView);
+        //setContentView(edtUsername);
+        mainContainerLayout.addView(edtUsername);
+
+        edtPassword = new EditText(this);
+        edtPassword.setLayoutParams(paramsForView);
+        mainContainerLayout.addView(edtPassword);
+
+        btnSignIn = new Button(this);
+        btnSignIn.setText("Sign In");
+        btnSignIn.setLayoutParams(paramsForView);
+        mainContainerLayout.addView(btnSignIn);
+
+        setContentView(mainContainerLayout);
+
+        btnSignIn.setOnClickListener(new BtnSignInClickListener());
     }
+
+    class BtnSignInClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if( edtUsername.getText().toString().equals("bitcode") && edtPassword.getText().toString().equals("bitcode@123") ) {
+                txtWelcomeMessage.setText("Welcome " + edtUsername.getText().toString());
+            }
+            else {
+                txtWelcomeMessage.setText("Login failed..");
+            }
+        }
+    }
+
 }
