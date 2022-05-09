7461601c5a615ba56953ca9fd8fc3b7353471405
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-05-09 13:19:32 +0530

    ImageView and accessing it through code.

--- app/src/main/java/in/bitcode/studentapp/LoginActivity.java
+++ app/src/main/java/in/bitcode/studentapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,6 +15,12 @@ public class LoginActivity extends Activity {
     private TextView txtWelcomeMessage;
     private EditText edtUsername, edtPassword;
     private Button btnLogin;
+    private ImageView imgCountryFlag;
+
+    private int [] imgCountryFlagIds = {
+            R.drawable.in_flag, R.drawable.au_flag, R.drawable.rs_flag
+    };
+    private int currentFlagIndex = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,13 +28,26 @@ public class LoginActivity extends Activity {
 
         setContentView(R.layout.login_activity);
 
+        imgCountryFlag = findViewById(R.id.imgCountryFlag);
         txtWelcomeMessage = findViewById(R.id.txtWelcomeMessage);
         edtUsername = findViewById(R.id.edtUsername);
         edtPassword = findViewById(R.id.edtPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
-        btnLogin.setOnClickListener(new BtnSignInClickListener());
+        imgCountryFlag.setImageResource(imgCountryFlagIds[currentFlagIndex]);
 
+        btnLogin.setOnClickListener(new BtnSignInClickListener());
+        imgCountryFlag.setOnClickListener(new ImgCountryFlagClickListener());
+
+    }
+
+    private class ImgCountryFlagClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            currentFlagIndex  = ++currentFlagIndex % imgCountryFlagIds.length;
+
+            imgCountryFlag.setImageResource(imgCountryFlagIds[currentFlagIndex]);
+        }
     }
 
     private class BtnSignInClickListener implements View.OnClickListener {
